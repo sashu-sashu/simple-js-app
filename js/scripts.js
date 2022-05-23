72950d24392370ae59b17824fc710d8f2a13178f
sashu-sashu <iamsashaalexandra@gmail.com>
2022-05-23 19:43:37 +0200

    array list created

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood = 'chocolate biscuits';
-document.write(favoriteFood);
+let pokemonList=[{name: "Misdreavus", hight: "2", type: "ghost"},{name: "Umbreon", hight: "1", type: "dark"},{name: "Smoochum", hight: "3", type: ["ice","psychic"]},{name: "Vileplume", hight: "3", type: ["grass","poison"]}];
+
+console.log(pokemonList)
+
