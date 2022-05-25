451399410232e1c0d8221fa638649903ac15a573
sashu-sashu <iamsashaalexandra@gmail.com>
2022-05-25 22:28:37 +0200

    for loop's code added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
-let pokemonList=[{name: "Misdreavus", hight: "2", type: "ghost"},{name: "Umbreon", hight: "1", type: "dark"},{name: "Smoochum", hight: "3", type: ["ice","psychic"]},{name: "Vileplume", hight: "3", type: ["grass","poison"]}];
+let pokemonList=[
+    {name: "Misdreavus", 
+    height: 2, 
+    type: ["ghost"]
+    },
 
-console.log(pokemonList)
+    {name: "Umbreon", 
+    height: 1, 
+    type: ["dark"]
+    },
 
+    {name: "Smoochum", 
+    height: 3, 
+    type: ["ice","psychic"]
+    },
+
+    {name: "Vileplume", 
+    height: 3, 
+    type: ["grass","poison"]
+    }
+    ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1 && pokemonList[i].height <3) { //code to highlight special Pokemon
+        document.write(pokemonList[i].name + " " + "(height: "+ pokemonList[i].height + ") Here is an average one!" + "<br />");
+    } else {
+        document.write(pokemonList[i].name + " " + "(height: "+ pokemonList[i].height +") <br />");
+      }
+    }
+    
