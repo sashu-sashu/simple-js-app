dcbd149980375ac1fb4d53ed7fbdd95e82b680b3
sashu-sashu <iamsashaalexandra@gmail.com>
2022-06-07 15:49:20 +0200

    for loop function changed to forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,17 @@ let pokemonList=[
     ];
 
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1 && pokemonList[i].height <3) { //code to highlight special Pokemon
-        document.write(pokemonList[i].name + " " + "(height: "+ pokemonList[i].height + ") Here is an average one!" + "<br />");
-    } else {
-        document.write(pokemonList[i].name + " " + "(height: "+ pokemonList[i].height +") <br />");
-      }
-    }
-    
+///for loop function
+///for (let i = 0; i < pokemonList.length; i++) {
+    ///if (pokemonList[i].height > 1 && pokemonList[i].height <3) { //code to highlight special Pokemon
+        ///document.write(pokemonList[i].name + " " + "(height: "+ pokemonList[i].height + ") Here is an average one!" + "<br />");
+    ///} else {
+        ///document.write(pokemonList[i].name + " " + "(height: "+ pokemonList[i].height +") <br />");
+      ///}
+    ///}
+
+///forEach function
+
+    pokemonList.forEach(function(pokemon){
+    document.write(pokemon.name + ": " + pokemon.type + "; " + pokemon.height + "<br />");
+  });
